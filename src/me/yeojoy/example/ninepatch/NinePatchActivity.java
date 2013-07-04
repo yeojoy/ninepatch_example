7de3772b1361a64c627ed9305d6bc1ad44abbbb7
yeojoy <yeojoy.kim@gmail.com>
2013-07-05 00:58:43 +0900

    first commit

---
+++ src/me/yeojoy/example/ninepatch/NinePatchActivity.java
@@ -0,0 +1,13 @@
+package me.yeojoy.example.ninepatch;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NinePatchActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
