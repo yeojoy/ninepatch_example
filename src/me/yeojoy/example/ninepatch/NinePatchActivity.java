60c660d2f261fa3de254ab852852929633ed07f9
yeojoy <yeojoy.kim@gmail.com>
2013-07-05 01:00:16 +0900

    add project.properties

--- src/me/yeojoy/example/ninepatch/NinePatchActivity.java
+++ src/me/yeojoy/example/ninepatch/NinePatchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 
 public class NinePatchActivity extends Activity {
     /** Called when the activity is first created. */
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
